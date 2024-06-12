7488b476f350fabf453b876063764c0155f2a45f
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2024-06-12 16:46:02 -0400

    Nav Bar updates

--- cooperativelearning/components/navBar.js
+++ cooperativelearning/components/navBar.js
@@ -1,14 +1,25 @@
 import Link from 'next/link';
 import '../src/app/globals.css'
+import Image from 'next/image'
+import logo from '../public/Logo.png';
 
 
 export default function NavBar() {
-return(
 
-    <nav className="p-4" style={{background:'#0C0C1D'}}>
-        
+
+  return(
+
+    <nav className="p-4 flex justify-start items-center" style={{background:'#0C0C1D', justifyContent:'space-between'}}>
+
+<div className='flex items-center gap-4'>
+    <Image className='relative z-10' src={logo} alt="Logo" width={41} />
+    <h1 className='text-white' style={{fontSize:'1.875rem'}}>Learn Together</h1>
+    </div>
+
+
+       
     <ul className="flex space-x-4" style={{justifyContent:'right'}}>
-        
+    
       <li>
         <Link href="/contact" className="text-white">
           Contact
