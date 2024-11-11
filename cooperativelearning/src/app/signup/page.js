e2b87d569f923b90054f436242785b60467aa4bb
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2024-11-11 13:31:03 -0500

    Styling Login and Sing up pages

--- cooperativelearning/src/app/signup/page.js
+++ cooperativelearning/src/app/signup/page.js
@@ -14,6 +14,8 @@ const Signup = () => {
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
 
+  const inputStyle = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-[800px]';
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
@@ -34,11 +36,11 @@ const Signup = () => {
   };
 
   return (
-    <div style={{ maxWidth: '400px', margin: '0 auto', padding: '1rem' }}>
-      <h2>Sign Up</h2>
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 items-center h-screen '>
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create your Account</h2>
       <form onSubmit={handleSubmit}>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="username">Username:</label>
+        <div style={{ marginBottom: '1rem', width: '350px' }}>
+          <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username:</label>
           <input
             type="text"
             id="username"
@@ -46,10 +48,12 @@ const Signup = () => {
             onChange={(e) => setUsername(e.target.value)}
             style={{ width: '100%', padding: '0.5rem' }}
             required
+            className= {inputStyle}
+            placeholder='Type your Username'
           />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="email">Email:</label>
+        
+        
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email:</label>
           <input
             type="email"
             id="email"
@@ -57,10 +61,12 @@ const Signup = () => {
             onChange={(e) => setEmail(e.target.value)}
             style={{ width: '100%', padding: '0.5rem' }}
             required
+            className= {inputStyle}
+            placeholder='Type your Email Address'
           />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="password">Password:</label>
+     
+        
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password:</label>
           <input
             type="password"
             id="password"
@@ -68,10 +74,12 @@ const Signup = () => {
             onChange={(e) => setPassword(e.target.value)}
             style={{ width: '100%', padding: '0.5rem' }}
             required
+            className= {inputStyle}
+            placeholder='Type a safe Password'
           />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="displayName">Display Name:</label>
+      
+        
+          <label htmlFor="displayName" className="block text-sm font-medium leading-6 text-gray-900">Display Name:</label>
           <input
             type="text"
             id="displayName"
@@ -79,21 +87,25 @@ const Signup = () => {
             onChange={(e) => setDisplayName(e.target.value)}
             style={{ width: '100%', padding: '0.5rem' }}
             required
+            className= {inputStyle}
+            placeholder='Type your First and Last Name'
           />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="phone">Phone Number:</label>
+       
+      
+          <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">Phone Number:</label>
           <input
             type="text"
             id="phone"
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
             style={{ width: '100%', padding: '0.5rem' }}
+            className= {inputStyle}
+            placeholder='Type your phone number'
           />
-        </div>
+       </div>
         {error && <p style={{ color: 'red' }}>{error}</p>}
         {success && <p style={{ color: 'green' }}>{success}</p>}
-        <button type="submit" style={{ padding: '0.5rem 1rem' }}>
+        <button type="submit" style={{ padding: '0.5rem 1rem' }}className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
           Sign Up
         </button>
       </form>
