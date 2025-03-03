7775e6bf545e49f7a8aebe00fbc7b30d7a7dee1c
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2025-03-03 14:52:34 -0500

    Sign in Component improved

--- cooperativelearning/src/app/login/page.js
+++ cooperativelearning/src/app/login/page.js
@@ -35,9 +35,9 @@ export default function Login() {
   return (
    <>
     <NavBar />
-   <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 items-center h-screen">
+   <div className="flex min-h-full flex-1 flex-col  px-6 py-12 lg:px-8 items-center ">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
